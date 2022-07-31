0a5af91890df20671f3c8622658f50c55f15ebc7
Klimovich Yauhenia <zh.shuravina@gmail.com>
2022-07-31 16:23:04 +0300

    add function if in code example 2

--- jscalculator/script.js
+++ jscalculator/script.js
@@ -3,48 +3,47 @@ var buttonMinus = document.getElementById('buttonMinus');
 var buttonMultiply = document.getElementById('buttonMultiply');
 var buttonDevide = document.getElementById('buttonDevide');
 
+var input1 = document.getElementById('number1');
+var input2 = document.getElementById('number2');
+
+function getNumber1() {
+    return Number(input1.value);
+}
+
+function getNumber2() {
+    return Number(input2.value);
+}
+
+function makeOperation(codeOperation) {
+    if (codeOperation === "+") {
+        var result = getNumber1() + getNumber2();
+    } else if (codeOperation === "-") {
+        var result = getNumber1() - getNumber2();
+    } else if (codeOperation === "*") {
+        var result = getNumber1() * getNumber2();
+    } else if (codeOperation === "/") {
+        var result = getNumber1() / getNumber2();
+    } else {
+        window.alert('operation is anknow');
+    }
+    
+    window.alert(result);
+}
+
 function onButtonPlusClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-
-    var summ = number1 + number2;
-    window.alert(summ);
+    makeOperation('+');
 }
 
 function onButtonMinusClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-
-    var summ = number1 - number2;
-    window.alert(summ);
+    makeOperation('-');
 }
 
 function onButtonMultiplyClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-
-    var summ = number1 * number2;
-    window.alert(summ);
+    makeOperation('*');
 }
 
 function onButtonDevideClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-
-    var summ = number1 / number2;
-    window.alert(summ);
+    makeOperation('/');
 }
 buttonPlus.addEventListener('click', onButtonPlusClick);
 buttonMinus.addEventListener('click', onButtonMinusClick);
